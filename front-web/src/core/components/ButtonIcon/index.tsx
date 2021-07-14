451784e39b46b4143885cbd09603dbe6984bdc67
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-14 19:58:54 -0300

    add home page

---
+++ front-web/src/core/components/ButtonIcon/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ReactComponent as ArrowICon } from '../../assets/images/arrow.svg'
+
+import './styles.scss'
+
+type Props = {
+    text?: string
+}
+
+const ButtonIcon = ( {text}:Props) => (
+    <div className="d-flex">
+        <button className="btn btn-primary btn-icon">
+            <h5>{text}</h5>
+        </button>
+        <div className="btn-icon-content">
+            <ArrowICon />
+        </div>
+    </div>
+);
+
+export default ButtonIcon;
