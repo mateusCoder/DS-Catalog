46a50bc16dc6aa2fc3557fc69fe38b99811e612e
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-17 14:50:00 -0300

    Add dynamic loader

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -8,6 +8,8 @@ import './styles.scss'
 import { useEffect } from 'react'
 import { makeRequest } from 'core/utils/request'
 import { Product } from 'core/types/Product'
+import ProductInfoLoader from '../Loaders/ProductInfoLoader'
+import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader'
 
 type ParamsType = {
     productId: string;
@@ -16,12 +18,13 @@ type ParamsType = {
 const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
     const [product, setProduct] = useState<Product>();
-
-    console.log(product)
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true)
         makeRequest({ url: `/products/${productId}` })
-            .then((response) => setProduct(response.data));
+            .then((response) => setProduct(response.data))
+            .finally(() => setIsLoading(false))
     }, [productId]);
 
     return (
@@ -33,21 +36,29 @@ const ProductDetails = () => {
                 </Link>
                 <div className="row">
                     <div className="col-6 pr-5">
-                        <div className="product-details-card text-center">
-                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
-                        </div>
-                        <h1 className="product-details-name">
-                            {product?.name}
-                        </h1>
-                        { product?.price && <ProductPrice price={product?.price}/>}
+                        {isLoading ? <h1><ProductInfoLoader /></h1> : (
+                            <>
+                                <div className="product-details-card text-center">
+                                    <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
+                                </div>
+                                <h1 className="product-details-name">
+                                    {product?.name}
+                                </h1>
+                                {product?.price && <ProductPrice price={product?.price} />}
+                            </>
+                        )}
                     </div>
                     <div className="col-6 product-details-card">
-                        <h1 className="product-description-title">
-                            Descrição do produto
-                        </h1>
-                        <p className="product-description-text">
-                            {product?.description}
-                        </p>
+                        {isLoading ? <h1><ProductDescriptionLoader /></h1> : (
+                            <>
+                                <h1 className="product-description-title">
+                                    Descrição do produto
+                                </h1>
+                                <p className="product-description-text">
+                                    {product?.description}
+                                </p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
