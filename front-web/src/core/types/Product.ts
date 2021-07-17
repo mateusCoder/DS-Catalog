10b6d058de521645f94d098dacdabdb448c14b31
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-17 14:06:27 -0300

    Add product listing with backend

---
+++ front-web/src/core/types/Product.ts
@@ -0,0 +1,19 @@
+export type ProductsReponse = {
+    content: Product[],
+    totalPages: number
+}
+
+export type Product = {
+    id: number,
+    name: string,
+    description: string,
+    price: number, 
+    imgUrl: string,
+    date: string,
+    categories: Category[]
+}
+
+export type Category = {
+    id:  number,
+    name: string
+}
