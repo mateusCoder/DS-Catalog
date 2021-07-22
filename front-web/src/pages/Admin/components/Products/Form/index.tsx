9423c19e76612ca7bb821f6413707333ca1aa58c
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-22 10:11:21 -0300

    Add form in sync with backend

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,16 +1,93 @@
+import { makeRequest } from 'core/utils/request'
 import React from 'react'
+import { useState } from 'react'
 import BaseForm from '../../BaseForm'
 import './styles.scss'
 
+type FormState = {
+    name: string,
+    price: string,
+    category: string,
+    description: string
+}
+
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+
 const Form = () => {
+    const [formData, setFormData] = useState<FormState>({
+        name: 'Computador',
+        price: '',
+        category: 'eletronicos',
+        description: ''
+    })
+
+    const handleOnChange = (event: FormEvent) => {
+        const name = event.target.name
+        const value = event.target.value
+
+        setFormData(data => ({ ...data, [name]: value}))
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const payLoad = {
+            ...formData,
+            imgUrl: 'https://images-americanas.b2w.io/produtos/01/00/img/2117190/5/2117190565_1GG.jpg',
+            categories: [{ id: formData.category }]
+        }
+
+        makeRequest({ url: '/products', method: 'POST', data: payLoad })
+        .then(() => {
+            setFormData({name: '', category: '', price: '', description: ''})
+        })
+    }
+
     return (
-        <BaseForm title="CADASTRAR UM PRODUTO">
-            <div className="row">
-                <div className="col-6">
-                    <input type="text" className="form-control" />
+        <form onSubmit={handleSubmit}>
+            <BaseForm title="CADASTRAR UM PRODUTO">
+                <div className="row">
+                    <div className="col-6">
+                        <input 
+                            value={formData.name}
+                            name="name"
+                            type="text" 
+                            className="form-control mb-5" 
+                            onChange={handleOnChange}
+                            placeholder="Nome do produto"
+                        />
+                        <select 
+                            value={formData.category}
+                            name="category"
+                            className="form-control mb-5" 
+                            onChange={handleOnChange}
+                        >
+                            <option value="1">Livros</option>
+                            <option value="2">Computadores</option>
+                            <option value="3">Eletrônicos</option>
+                        </select>
+                        <input 
+                            value={formData.price}
+                            name="price"
+                            type="text" 
+                            className="form-control" 
+                            onChange={handleOnChange}
+                            placeholder="Preço"
+                        />
+                    </div>
+                    <div className="col-6">
+                        <textarea 
+                            name="description"
+                            value={formData.description}
+                            onChange={handleOnChange} 
+                            className="form-control"
+                            id="" 
+                            cols={30} 
+                            rows={10}
+                        />
+                    </div>
                 </div>
-            </div>
-        </BaseForm>
+            </BaseForm>
+        </form>
     )
 }
 
