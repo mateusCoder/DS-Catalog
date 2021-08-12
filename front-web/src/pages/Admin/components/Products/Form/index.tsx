875f01a6b0955c890ba9dc776076a850e6c9ce34
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-08-12 20:16:10 -0300

    Add Select from react-select

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
-import BaseForm from '../../BaseForm'
-import './styles.scss'
+import Select from 'react-select'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify';
 import { makePrivateRequest, makeRequest } from 'core/utils/request'
 import { useHistory, useParams } from 'react-router-dom';
+import BaseForm from '../../BaseForm'
+import './styles.scss'
 
 type FormState = {
     name: string,
@@ -16,6 +17,12 @@ type FormState = {
 type ParamsType = {
     productId: string;
 }
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+]
 
 const Form = () => {
     const { register, handleSubmit, formState: { errors }, setValue } = useForm<FormState>()
@@ -73,6 +80,14 @@ const Form = () => {
                                     Nome inválido
                                 </div>
                             )}
+                        </div>
+                        <div className="margin-bottom-30">
+                            <Select 
+                                options={options} 
+                                classNamePrefix="categories-select"
+                                placeholder= 'Categoria'
+                                isMulti
+                            />
                         </div>
                         <div className="margin-bottom-30">
                             <input
